03803ea11aee2a7dca7888b491c35ff4c0c40db9
Jeremy Ang <jang025@users.noreply.github.com>
2025-01-19 23:55:29 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-violet-300 font-bold">Zentry Clone</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 };
